2165afe81951abcb0397b805564915221631d2a5
Tim Lohrer <me@timlohrer.de>
2024-12-01 18:56:21 +0100

    fix icon sizes

--- lib/utils/NoRiskIcon.dart
+++ lib/utils/NoRiskIcon.dart
@@ -2,23 +2,23 @@ import 'package:flutter/cupertino.dart';
 
 class NoRiskIcon {
   static Image delete = Image.asset('lib/assets/icons/delete.png',
-      height: 20, width: 15, fit: BoxFit.fill);
+      height: 22.5, width: 17.5, fit: BoxFit.fill);
   static Image comment = Image.asset('lib/assets/icons/comment.png',
-      height: 25, width: 25, fit: BoxFit.fill);
+      height: 30, width: 30, fit: BoxFit.fill);
   static Image back = Image.asset('lib/assets/icons/back.png',
-      height: 20, width: 27.5, fit: BoxFit.fill);
+      height: 30, width: 30, fit: BoxFit.fill);
   static Image upvote = Image.asset('lib/assets/icons/upvote.png',
-      height: 20, width: 20, fit: BoxFit.fill);
+      height: 17.5, width: 20, fit: BoxFit.fill);
   static Image downvote = Image.asset('lib/assets/icons/downvote.png',
-      height: 20, width: 20, fit: BoxFit.fill);
+      height: 17.5, width: 20, fit: BoxFit.fill);
   static Image upvoted = Image.asset('lib/assets/icons/upvoted.png',
-      height: 20, width: 20, fit: BoxFit.fill);
+      height: 17.5, width: 20, fit: BoxFit.fill);
   static Image downvoted = Image.asset('lib/assets/icons/downvoted.png',
-      height: 20, width: 20, fit: BoxFit.fill);
+      height: 17.5, width: 20, fit: BoxFit.fill);
   static Image reload = Image.asset('lib/assets/icons/reload.png',
       height: 25, width: 25, fit: BoxFit.fill);
   static Image report = Image.asset('lib/assets/icons/report.png',
-      height: 25, width: 25, fit: BoxFit.fill);
+      height: 30, width: 30, fit: BoxFit.fill);
   static Image lock = Image.asset('lib/assets/icons/lock.png',
       height: 20, width: 15, fit: BoxFit.fill);
   static Image streak = Image.asset('lib/assets/icons/fire.webp',
