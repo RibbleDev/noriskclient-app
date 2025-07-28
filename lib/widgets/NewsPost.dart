30c5b0e3c83fbba072da5d8a9f6ee8e314c15b6c
Tim Lohrer <me@timlohrer.de>
2025-07-28 20:52:57 +0200

    feat: add news link

--- lib/widgets/NewsPost.dart
+++ lib/widgets/NewsPost.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:noriskclient/config/Colors.dart';
 import 'package:noriskclient/widgets/NoRiskContainer.dart';
 import 'package:noriskclient/widgets/NoRiskText.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class NewsPost extends StatelessWidget {
   final String title;
@@ -22,25 +22,30 @@ class NewsPost extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NoRiskContainer(
-      color: isNewest ? NoRiskClientColors.blue : Colors.white,
-      padding: const EdgeInsets.only(top: 2.5, bottom: 10, left: 10, right: 10),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              NoRiskText('${title.toLowerCase()} - $postedAt',
-                  spaceTop: false,
-                  spaceBottom: false,
-                  style: TextStyle(fontSize: 25, color: NoRiskClientColors.text)),
-            ],
-          ),
-          const SizedBox(height: 5),
-          SizedBox(
-            width: MediaQuery.of(context).size.width - 2 * 15 - 2 * 10,
-            child: Image.network(imageUrl, fit: BoxFit.cover),
-          ),
-        ],
+    return GestureDetector(
+      onTap: () => launchUrlString(link, mode: LaunchMode.externalApplication),
+      child: NoRiskContainer(
+        color: isNewest ? NoRiskClientColors.blue : Colors.white,
+        padding:
+            const EdgeInsets.only(top: 2.5, bottom: 10, left: 10, right: 10),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                NoRiskText('${title.toLowerCase()} - $postedAt',
+                    spaceTop: false,
+                    spaceBottom: false,
+                    style: TextStyle(
+                        fontSize: 25, color: NoRiskClientColors.text)),
+              ],
+            ),
+            const SizedBox(height: 5),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 2 * 15 - 2 * 10,
+              child: Image.network(imageUrl, fit: BoxFit.cover),
+            ),
+          ],
+        ),
       ),
     );
   }
